95962c4bb0a3e0f522032e08c74c569057ba523a
Pamela Alvarez <p.stacey.a@gmail.com>
2016-07-16 03:36:28 +0000

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,43 @@
+
+
 def even_next(n)
+       n / 2
 end
+
 
 def odd_next(n)
+     3 * n + 1
 end
+
 
 def next_value(n)
+  if n.even?
+      n / 2
+    elsif    n.odd?
+        3 * n + 1
+   end
 end
 
-def collatz(n)
+
+
+ def collatz(n)
+    list = [n]
+
+    until n == 1
+         n = next_value(n)
+         list.push(n)
+        end
+       return list
 end
+
+
 
 def longest_collatz
+  hashes = {}
+  range = (80000..1000000)
+  	range.each do |n|
+  		length = collatz(n).length
+  		hashes[length] = n
+  	end
+  	hashes.sort.last[1]
 end
